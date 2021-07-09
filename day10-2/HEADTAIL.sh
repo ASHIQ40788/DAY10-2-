0fb50cb120f39e5f3ec8de069f4e08e2c5fa1e5c
ASHIQ40788 <ashiqshaiksvali8@gmail.com>
2021-07-09 19:19:06 +0530

    Added:HEADTAIL

--- day10-2/HEADTAIL.sh
+++ day10-2/HEADTAIL.sh
@@ -1,16 +1,13 @@
 #!/bin/bash -x
 
-flips = 0
-heads = 0
-tails = 0
-while flips < 100:
-    if random.randint(1,2) == 1:
-        print("heads")
-        heads += 1
-    else:
-        print("tails")
-        tails += 1
-        flips += 1
-
-print("you got ", heads," heads, and ", tails," tails!")
-input ("exit")
+tail=10
+head=10
+if [ $head -gt $tail ]
+then
+echo "$head is more  wins than $tail"
+elif [ $head -eq $tail ]
+then
+echo "head and tails are equal wins"
+else 
+echo "$head is less wins than $tail"
+fi
